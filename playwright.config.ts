ef523e0462448ea484eef3f96df98785311165cf
Grazyna Borowka-Magda <gow@spyro-soft.com>
2026-08-10 15:20:42 +0200

    feat: add mobile Chrome test project

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
   use: {
     baseURL: 'http://localhost:4321',
   },
+  projects: [
+    {
+      name: 'desktop-chrome',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'mobile-chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+  ],
 });
